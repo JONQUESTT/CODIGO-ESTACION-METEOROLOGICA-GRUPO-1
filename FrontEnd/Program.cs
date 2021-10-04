69d35ad209b9467854b7619c6ded128104cd92b3
jonquestt <jonquestt@gmail.com>
2021-10-04 14:41:35 -0500

    integracion frontend

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -6,14 +6,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Aplicacion.App.Dominio;
+using Aplicacion.App.Persistencia;
 
 namespace FrontEnd
 {
     public class Program
     {
+        private static IRepositorioPersona _repoPersona= new RepositorioPersona(new Aplicacion.App.Persistencia.AppContext());
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //InsertarPersona();
+            //_repoPersona.DeletePersona("9999");
+        }
+        private static void InsertarPersona()
+        {
+            var p=new Persona
+            {
+                Identificacion="1234",
+                Nombre="Ana",
+                Apellido="Arcos",
+                Genero="Femenino",
+		        Estado= 'A'
+        };
+            _repoPersona.AddPersona(p);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
