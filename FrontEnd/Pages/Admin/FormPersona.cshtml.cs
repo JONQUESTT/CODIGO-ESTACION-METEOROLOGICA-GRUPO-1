5aab1fa6a307d1143f0a07a6a0519acecca1c430
Catalina Escobar <escobarcatalina@gmail.com>
2021-10-06 21:32:39 -0500

    Inicio Integración
    Inicio de Integración de formulario persona

--- FrontEnd/Pages/Admin/FormPersona.cshtml.cs
+++ FrontEnd/Pages/Admin/FormPersona.cshtml.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Aplicacion.App.Dominio;
+using Aplicacion.App.Persistencia:
 
 namespace MyApp.Namespace
 {
+    
     public class FormPersonaModel : PageModel
     {
+        public static IRepositorioPersona _repoPersona = new RepositorioPersona(new Aplicacion.App.Persistencia.AppContext());
         public void OnGet()
         {
         }
+
+        public void OnPost(string TipoPersona,string Nombre,string Apellido,string Identificacion,string Correo,string Genero,DateTime FechaIngreso,string Cargo,string Turno,char EstadoPersona,int IDTecnico)
+        {
+            var persona = new Persona();
+            persona.Identificacion = Identificacion;
+            persona.Nombres = Nombre;
+            persona.Apellidos = Apellido;
+            persona.Genero = Genero;
+            persona.Estado = EstadoPersona; 
+            _repoPersona.AddPersona(persona);
+        }
+
+
     }
 }
